bcd55236926497aaac5a2c36946ec96ba939ef83
Yangkai.Shen <237497819@qq.com>
2019-08-14 17:41:17 +0800

    :sparkles: 添加原型模式，深克隆

---
+++ src/main/java/com/xkcoding/design/pattern/creational/prototype/deepclone/HundredMoney.java
@@ -0,0 +1,50 @@
+package com.xkcoding.design.pattern.creational.prototype.deepclone;
+
+import com.xkcoding.design.pattern.creational.prototype.Money;
+import com.xkcoding.design.pattern.creational.prototype.Shape;
+import lombok.Cleanup;
+import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.io.*;
+
+/**
+ * <p>
+ * 一百元
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-08-14 15:56
+ */
+@Data
+public class HundredMoney implements Money, Cloneable, Serializable {
+    private Shape shape;
+
+    /**
+     * 打印
+     *
+     * @return {@link Money}
+     */
+    @Override
+    public Money print() {
+        return (Money) this.clone();
+    }
+
+    @Override
+    protected Object clone() {
+        return this.deepClone();
+    }
+
+    @SneakyThrows
+    private Object deepClone() {
+        @Cleanup ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        @Cleanup ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+
+        @Cleanup ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        @Cleanup ObjectInputStream ois = new ObjectInputStream(bis);
+
+        return ois.readObject();
+    }
+
+}
---
+++ src/main/java/com/xkcoding/design/pattern/creational/prototype/deepclone/run/PatternTest.java
@@ -0,0 +1,26 @@
+package com.xkcoding.design.pattern.creational.prototype.deepclone.run;
+
+import com.xkcoding.design.pattern.creational.prototype.Shape;
+import com.xkcoding.design.pattern.creational.prototype.deepclone.HundredMoney;
+
+/**
+ * <p>
+ * 原型模式，深克隆测试，注意所有引用对象均需要实现 {@link java.io.Serializable} 接口
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-08-14 17:29
+ */
+public class PatternTest {
+    public static void main(String[] args) {
+        HundredMoney money1 = new HundredMoney();
+        money1.setShape(new Shape("纸币"));
+
+        // 原型模式 深克隆
+        HundredMoney money2 = (HundredMoney) money1.print();
+
+        System.out.println("money1 -> 引用对象地址：" + money1.getShape());
+        System.out.println("money2 -> 引用对象地址：" + money2.getShape());
+        System.out.println("引用对象地址比较：" + (money1.getShape() == money2.getShape()));
+    }
+}
