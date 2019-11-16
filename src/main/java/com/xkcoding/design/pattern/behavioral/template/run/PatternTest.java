9593aefab4fb5d7c91eeb74e37adf4da5cd6e9a8
Yangkai.Shen <237497819@qq.com>
2019-11-16 17:13:01 +0800

    :bulb: 添加模板模式

---
+++ src/main/java/com/xkcoding/design/pattern/behavioral/template/run/PatternTest.java
@@ -0,0 +1,15 @@
+package com.xkcoding.design.pattern.behavioral.template.run;
+
+/**
+ * <p>
+ * 模板模式，测试类
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019/11/18 17:10
+ */
+public class PatternTest {
+    public static void main(String[] args) {
+
+    }
+}
