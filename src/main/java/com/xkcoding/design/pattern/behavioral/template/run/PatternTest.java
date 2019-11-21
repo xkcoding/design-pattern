920e6c6315c6a56ca5fc3363c216a3cd89201e35
Yangkai.Shen <237497819@qq.com>
2019-11-21 21:24:14 +0800

    :bulb: 添加源码注释

--- src/main/java/com/xkcoding/design/pattern/behavioral/template/run/PatternTest.java
+++ src/main/java/com/xkcoding/design/pattern/behavioral/template/run/PatternTest.java
@@ -13,9 +13,11 @@ import com.xkcoding.design.pattern.behavioral.template.SmsSender;
  */
 public class PatternTest {
     public static void main(String[] args) {
+        // 邮件发送
         EmailSender emailSender = new EmailSender();
         emailSender.send("测试内容");
 
+        // 短信发送
         SmsSender smsSender = new SmsSender();
         smsSender.send("17312341234");
     }
